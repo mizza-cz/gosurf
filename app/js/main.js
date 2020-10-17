fc01ac5d8866fe3068fdc867e20dacae99d7259e
mizza-cz <mizza.cz666@gmail.com>
2020-10-17 20:43:13 +0200

    add surf section

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,27 @@ $(function () {
       fade: true,
       prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
       nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
-      asNavFor: '.slider-dots',
+      asNavFor: '.slider-dotshead',
    });
-   $('.slider-dots').slick({
+   $('.slider-dotshead').slick({
       slidesToShow: 4,
       slidesToScroll: 4,
       asNavFor: '.header__slider',
 
+   });
+   $('.surf-slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+      nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+      asNavFor: '.slider-map',
+   });
+
+   $('.slider-map').slick({
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.surf-slider',
+      focusOnSelect: true,
    });
 });
