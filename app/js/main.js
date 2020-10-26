3df4b2e919bdb5e061ca0c2011d690a4ca43d8f6
mizza-cz <mizza.cz666@gmail.com>
2020-10-26 13:03:03 +0100

    add animate

--- app/js/main.js
+++ app/js/main.js
@@ -147,7 +147,6 @@ $(function () {
 
    $('.menu-btn').on('click', function () {
       $('.menu').toggleClass('active');
-
-
    });
+   new WOW().init();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ gulp.task('style', function () {
 
 gulp.task('script', function () {
    return gulp.src([
+      'node_modules/wow.js/dist/wow.js',
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
    ])
